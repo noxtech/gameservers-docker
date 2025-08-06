697ef95421becaba96f0ac235474341dbc1facd4
Matthew Hayward <matthewh86@gmail.com>
2025-08-06 22:24:41 +0100

    update ut2004

--- ut2004/download.sh
+++ ut2004/download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o https://www.utzone.de/forum/downloads.php?do=file&id=1196&act=down
+curl -o dedicatedserver3369.3-bonuspack.zip -L https://archive.org/download/ut2004-server/dedicatedserver3369.3-bonuspack.zip
