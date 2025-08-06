283cc835bd65b3f0bd3b08cd9c907ac1fea890d4
Matthew Hayward <matthewh86@gmail.com>
2025-08-06 22:20:51 +0100

    update scpsl-exiled

--- scpsl-exiled/download.sh
+++ scpsl-exiled/download.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
+source /steam/scripts/zip-utils.sh
+
 set -e
 
-# https://github.com/Exiled-Team/EXILED/releases
-
-curl -LO 'https://github.com/Exiled-Team/EXILED/releases/download/8.9.11/Exiled.tar.gz'
+# https://github.com/ExMod-Team/EXILED/releases
+VERSION=$(curl -s https://api.github.com/repos/ExMod-Team/EXILED/releases/latest | jq -r .tag_name)
+curl -LO "https://github.com/ExMod-Team/EXILED/releases/download/${VERSION}/Exiled.tar.gz"
 tar -xzvf Exiled.tar.gz
 cp -r EXILED "${CONFIG_DIR}"
 rm -fr EXILED
 
-# https://github.com/Exiled-Team/Common-Utils
- curl -LO 'https://github.com/Exiled-Team/Common-Utils/releases/download/7.1.1/Common.Utilities.dll'
+# https://github.com/ExMod-Team/Common-Utils
+VERSION=$(curl -s https://api.github.com/repos/ExMod-Team/Common-Utils/releases/latest | jq -r .tag_name)
+curl -LO "https://github.com/ExMod-Team/Common-Utils/releases/download/${VERSION}/Common.Utilities.dll"
 
-# https://github.com/Michal78900/RespawnTimer
- curl -LO 'https://github.com/Michal78900/RespawnTimer/releases/download/v4.0.4/RespawnTimer.dll'
+# https://github.com/LumiFae/Timers/
+VERSION=$(curl -s https://api.github.com/repos/LumiFae/Timers/releases/latest | jq -r .tag_name)
+curl -LO "https://github.com/LumiFae/Timers/releases/download/${VERSION}/Timers-EXILED-HSM.dll"
 
-# https://github.com/babyboucher/RPNames
- curl -LO 'https://github.com/1EnesBaturKaza/RPNames/releases/download/v2.2.0/RPNames.dll'
+# https://github.com/Konoaru384/RoleDisplayPlugin
+VERSION=$(curl -s https://api.github.com/repos/Konoaru384/RoleDisplayPlugin/releases/latest | jq -r .tag_name)
+curl -LO "https://github.com/Konoaru384/RoleDisplayPlugin/releases/download/${VERSION}/plugins.zip"
+unzipAndMoveToDir plugins.zip "${CONFIG_DIR}/EXILED/Plugins/"
 
-# https://github.com/DGvagabond/SCP-008-X
-curl -LO 'https://github.com/DGvagabond/SCP-008-X/releases/download/3.1.2/SCP008X.dll'
+# https://github.com/MeowServer/HintServiceMeow/
+VERSION=$(curl -s https://api.github.com/repos/MeowServer/HintServiceMeow/releases/latest | jq -r .tag_name)
+curl -LO "https://github.com/MeowServer/HintServiceMeow/releases/download/${VERSION}/HintServiceMeow-Exiled.dll"
 
 mkdir -p "${CONFIG_DIR}/EXILED/Plugins/"
 mv *.dll "${CONFIG_DIR}/EXILED/Plugins/"
+rm -f *.zip
